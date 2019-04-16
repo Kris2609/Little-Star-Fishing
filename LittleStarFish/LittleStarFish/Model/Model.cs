54d77e4f13abb809e210f05f2d9854b19bb5f453
Kristoffer <Stoffer26@hotmail.com>
2019-04-16 12:29:00 +0200

    database sqlite added

--- LittleStarFish/LittleStarFish/Model/Model.cs
+++ LittleStarFish/LittleStarFish/Model/Model.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SQLite;
 
 namespace LittleStarFish
 {
+    
     public class Model
     {
+        private const String CONNECTIONSTRING = @"Data Source=testtabel.db;version=3;New=true;Compress=true";
+        private SQLiteConnection connection;
+        public Model()
+        {
+            connection = new SQLiteConnection(CONNECTIONSTRING);
+            connection.Open();
+        }
     }
 }
