da6313061df79453b58c94fce3f94cfc7452ce9c
Kristoffer <Stoffer26@hotmail.com>
2019-04-16 11:26:10 +0200

    KAOS

--- LittleStarFish/LittleStarFish/Model/Player/Player.cs
+++ LittleStarFish/LittleStarFish/Model/Player/Player.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace LittleStarFish
 {
-    public class Bait
+    public class Player : GameObject
     {
     }
 }
--- LittleStarFish/LittleStarFish/View/GameState.cs
+++ LittleStarFish/LittleStarFish/View/GameState.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace LittleStarFish
 {
-    public class Fish
+    public class GameState
     {
     }
 }
